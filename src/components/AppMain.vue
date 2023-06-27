d974d988fb489ec5fe6ca137af61025e7c0a6000
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-27 11:56:01 +0200

    imported in AppMain component AppServices AppCompany and AppProjects components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,21 @@
 <template>
     <AppJumbotron/>
+    <AppServices/>
+    <AppCompany/>
+    <AppProjects/>
 </template>
 <script>
  import AppJumbotron from './AppJumbotron.vue';
+ import AppServices from './AppServices.vue';
+ import AppCompany from './AppCompany.vue';
+ import AppProjects from './AppProjects.vue';
 export default {
     name:'AppMain',
     components:{
-        AppJumbotron
+        AppJumbotron,
+        AppServices,
+        AppCompany,
+        AppProjects
     }
 }
 </script>
--- src/components/AppProjects.vue
+++ src/components/AppProjects.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <ProjectCard/>
     </div>
 </template>
 <script>
