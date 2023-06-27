de24e8ba19ca86f8cf59893e06c4f80fe2c72cf0
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-27 11:47:08 +0200

    added services card component and imported in AppServices component

--- src/components/AppServices.vue
+++ src/components/AppServices.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <ServiceCard/>
     </div>
 </template>
 <script>
+import ServiceCard from './ServiceCard.vue';
 export default {
-    
+    name:'AppServices',
+    components:{
+        ServiceCard
+    }
 }
 </script>
 <style lang="scss" scoped>
