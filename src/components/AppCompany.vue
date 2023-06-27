a45d6a1511053488927c68434cfce32692a5ca67
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-27 19:25:35 +0200

    added style to AppCompany component and added a new section to the component

--- src/components/AppCompany.vue
+++ src/components/AppCompany.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        
-    </div>
+    <section>
+        <div class="container">
+          <div class="description">
+            <h1>The Company</h1>
+          </div>
+          <div class="image"> 
+
+          </div>
+        </div>
+    </section>
 </template>
 <script>
 export default {
@@ -11,4 +18,29 @@ export default {
 <style lang="scss" scoped>
     @use '../styles/partials/mixins.scss' as*;
     @use '../styles/partials/variables.scss' as*;
+
+    section{
+        background: linear-gradient(45deg, rgb(13, 27, 42), rgb(34, 72, 100));
+    }
+    div.container{
+        height: 659px;
+        display: flex;
+        padding: 100px 0px;
+        width: 70%;
+        margin: 0 auto;
+       
+        div.description{
+            width: 40%;
+            
+            h1{
+                font-size: 48px;
+                color: white;
+            }
+        }
+
+        div.image{
+            width: 40%;
+        }
+       
+    }
 </style>
