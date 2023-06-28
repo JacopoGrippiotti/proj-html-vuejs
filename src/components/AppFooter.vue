7614290374b27383f2809313463f12eeaf1b86a1
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-28 17:13:07 +0200

    added menuList to data in AppFooter component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,25 @@
 </template>
 <script>
 export default {
-    name:'AppFooter'
+    name:'AppFooter',
+    data() {
+        return {
+            menuList:[
+                     {
+                      title:"About",
+                      listItems:["The Company","Institutional","Social & Events","Innovation","Environment","Technology"]
+                     },
+                     {
+                      title:"Services",
+                      listItems:["Audit & Assurance","Financial Advisory","Analytics M&A","Middle Marketing","Legal Consulting","Regulatory Risk"]
+                     },
+                     {
+                        title:"Support",
+                        listItems:["Responsibility","Terms of Use","About Cookies","Privacy Policy","Accessibility","Information"]
+                     }
+                ]
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
