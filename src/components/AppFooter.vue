8df3ac1077b4484760a42e890e60d40544e99034
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-28 17:26:25 +0200

    Structured FooterCard component to receive title and list props from AppFooter component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        
-    </div>
+    <section>
+        <div class="container">
+        <FooterCard v-for="card in menuList"
+        :title="card.title" :list="card.listItems"/>
+        </div>
+    </section>
+    
 </template>
 <script>
+import FooterCard from './FooterCard.vue';
 export default {
     name:'AppFooter',
+    components:{
+        FooterCard
+    },
     data() {
         return {
             menuList:[
@@ -18,8 +26,8 @@ export default {
                       listItems:["Audit & Assurance","Financial Advisory","Analytics M&A","Middle Marketing","Legal Consulting","Regulatory Risk"]
                      },
                      {
-                        title:"Support",
-                        listItems:["Responsibility","Terms of Use","About Cookies","Privacy Policy","Accessibility","Information"]
+                      title:"Support",
+                      listItems:["Responsibility","Terms of Use","About Cookies","Privacy Policy","Accessibility","Information"]
                      }
                 ]
         }
--- src/components/FooterCard.vue
+++ src/components/FooterCard.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
-        
+    <div class="footer-card">
+        <h2>{{ title }}</h2>
+        <ul>
+            <li v-for="listItem in list">{{ listItem }}</li>
+        </ul>
     </div>
 </template>
 <script>
 export default {
+    name:'FooterCard',
+    props:{
+        title:String,
+        list:Array
+    },
     data() {
         return {
            
@@ -12,6 +20,11 @@ export default {
     },
 }
 </script>
-<style lang="">
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins.scss' as*;
+    @use '../styles/partials/variables.scss' as*;
     
+    div.footer-card{
+        width: calc(100% / 4) - 20px;
+    }
 </style>
