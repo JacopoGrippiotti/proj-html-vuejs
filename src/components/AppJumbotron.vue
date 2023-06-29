8d82e6d05cea25b8c4893b474f997daa3a97ba08
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-29 14:50:30 +0200

    structured and styled jumbotron component and imported in jumbotron component AppSlider component

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,14 +1,56 @@
 <template>
-    <div>
-        
-    </div>
+    <section class="jumbotron">
+        <AppSlider
+        :slideList="slideList"/>
+    </section>
 </template>
 <script>
+import AppSlider from './AppSlider.vue';
 export default {
-    name:'AppJumbotron'
+    name:'AppJumbotron',
+    components:{
+        AppSlider
+    },
+    data() {
+        return {
+            slideList:[
+                {
+                    title:"We Share",
+                    span:"Good Ideas",
+                    description:"Escape the comfort zone and achieve better results with the help of experts who understand the subject"
+                },
+                {
+                    title:"Ready",
+                    span:"Team",
+                    description:"No matter what your company needs, we will be ready to assist you in the best possible way."
+                },
+                {
+                    title:"Talk to a",
+                    span:"Consultant",
+                    description:"Do not miss the opportunity to achieve better results and solidity in the market"
+                }
+            ]
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
     @use '../styles/partials/mixins.scss' as*;
     @use '../styles/partials/variables.scss' as*;
+    
+    section.jumbotron{
+        height: 1000px;
+        background-image: url('../img/svgs/images/bg-parallax.png');
+        display: flex;
+        background-position-x:center ;
+    }
+
+    .background-center{
+        background-position-x: center;
+    }
+
+    .background-right{
+        background-position: right;
+    }
+    
 </style>
---
+++ src/components/AppSlider.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="slider">
+        <h1>
+            {{ slideList[activeIndex].title }} <span>{{ slideList[activeIndex].span }}</span>
+        </h1>
+        <p>{{ slideList[activeIndex].description }}</p>
+        <input type="button" value="Get in Touch">
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'AppSlider',
+    props:{
+        slideList:Array
+    },
+    data() {
+        return {
+            activeIndex:1
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+    div.slider{
+        width: 30%;
+        margin: auto;
+        text-align: center;
+
+        h1{
+            font-size: 72px;
+
+            span{
+                color: #058283;
+            }
+        }
+
+        p{
+            font-size: 19px;
+            color: #606d75;
+            font-weight: 400;
+            margin-top: 30px;
+        }
+
+        input{
+            padding: 10px 25px;
+            text-transform: uppercase;
+            font-size: 14px;
+            background-color: #058283;
+            border: 1px solid #058283;
+            border-radius: 4px 4px 4px 4px;
+            color: white;
+            text-align: center;
+            margin-top: 30px;
+            margin-bottom: 30
+        }
+    }
+</style>
